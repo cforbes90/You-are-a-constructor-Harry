2a53b2d3dfa52baf09ef53ba64eab7f81a0f290d
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-10-19 11:51:36 -0400

    finsihedpseudo coding and started progress on letter Constructor.

---
+++ index.js
@@ -0,0 +1,6 @@
+// Tasks to complete 
+//1 Prompts for user
+// 2 word array to choose from
+// 3 random function that will choose from that word array
+// 4 loop that spells out word using underscores and displays them
+//5 user guess unti correctly an dlogic for establishing win
