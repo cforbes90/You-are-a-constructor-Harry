0a768abf43a698a8c8fbaff125de9d008236b2ee
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-10-21 23:51:15 -0400

    Fixed mostly everything. Focusing on bug squashing and fringe cases.

--- index.js
+++ index.js
@@ -1,104 +1,87 @@
-// Tasks to complete 
-//1 Prompts for user
-// 2 word array to choose from
-// 3 random function that will choose from that word array
-// 4 loop that spells out word using underscores and displays them
-//5 user guess unti correctly an dlogic for establishing win
-// Display letters that have been guessed so far! push guessed letters to an array and then 
-//display that array
-var inquirer=require("inquirer");
-var word=require("./word");
-var letter=require("./letter");
-var wordlist=require("./wordlist");
-var userGuess="";
-var guessedGuestList=[];
-var guesses=3;
-var computerWord= new word(wordlist[Math.floor(Math.random()*wordlist.length)]);
+
+var inquirer = require("inquirer");
+var word = require("./word");
+var wordlist = require("./wordlist");
+var userGuess = "";
+var guessedGuestList = [];
+//var guesses = 3;
+var computerWord = new word(wordlist[Math.floor(Math.random() * wordlist.length)]);
 
 computerWord.makeWord();
-//for(i=0; i<computerWord.letterObjects.length; i++){
-    //console.log("the next thing is computerWord.letterObjects[i].char called from Index");
-//computerWord.letterObjects[i].char[0].spaceCheck();
-//console.log( computerWord.letterObjects[i].char);
-//}
-//console.log("the following is trying to drill down");
-//console.log(computerWord.letterObjects.char);
-//console.log('after running makeWord computerWord:', computerWord.letterObjects);
-//console.log(this.letterObjects);
-// console.log("This is word");
-// console.log(word);
-//console.log("next is word.char");
-//console.log(this.word.makeWord(computerWord));
-// this.showWordEndGame();
 
-// console.log("\x1b[1m", "\x1b[31m", "You failed...", "\x1b[0m");
-// console.log(" ___________");
-// console.log("/   R.I.P.  ⟍");
-// console.log("| HERE LIES  |");
-// console.log("|   ...      |");
-// console.log("|"+"\x1b[5m",  "YOU!!!!", "\x1b[0m   |");
-// console.log("|            |");
-// console.log(" ~~~~~~~~~~~~");
-function gameStart(){
-    console.log("*~~~*~~~*~~~*~~~*~~~*~~~*~~~*")
-    console.log("      Hello there!");
-    console.log("          _██_");
-    console.log("        ‹(•¿•)›");
-    console.log("       .. (█)");
-    console.log("       .../ I");
-    console.log("    Care for a game?");
-    console.log("*~~~*~~~*~~~*~~~*~~~*~~~*~~~*");
-    };
-
-// console.log('|guessedGuestList:', guessedGuestList+ "|");
-gameStart();
-function gameLoop(){
-    if (guesses>0){
-        console.log('User guesses remaining:', guesses);
-        //gameLoop();
+function gameLoop() {
+    if ((computerWord.guesses > 0) && (computerWord.display.length > computerWord.winning)) {
+        console.log('Guesses remaining:', computerWord.guesses);
         inquirer.prompt([
-         {
-                name:"charGuessed",
+            {
+                name: "charGuessed",
                 message: "Pick a letter!"
             }
-        ]).then(function(answer){
-            guesses--;
-            userGuess=answer.charGuessed;
-            console.log("The following is the userGuess:", userGuess);
-            for(i=0; i<computerWord.letterObjects.length;i++){
-                if(userGuess==computerWord.letterObjects[i].char){
-                    guesses++;
-                }
+        ]).then(function (answer) {
+            computerWord.subtractGuesses();
+            console.log("Below is computer Word guess after subtract Guesses");
+            console.log(computerWord.guesses);
+            userGuess = answer.charGuessed;
+            //console.log('guessedGuestList', guessedGuestList, typeof answer.charGuessed)
+            if (guessedGuestList.includes(answer.charGuessed.toUpperCase())) {
+                console.log("You already tried that letter. Guess again!")
+                computerWord.addGuesses();
+                //gameLoop();
             }
-            console.log('This is after the add life loop User guesses remaining:', guesses);
-            var upper=userGuess.toUpperCase();
-            console.log("The following is upper: ", upper);
+
+            console.log("You guessed: " + userGuess + "!");
+            
+            var upper = userGuess.toUpperCase();
+            //console.log("The following is upper: ", upper);
             computerWord.showWordGuess(upper);
             //console.log(computerWord.displayArray);
             guessedGuestList.push(upper);
-            console.log('Guessed Guest List:', guessedGuestList); 
-      
-       console.log("＼＼ ＿");
-       　　 console.log("＼(　•_•) F");
-       　　　console.log(" <　⌒ヽ A");
-       　　　console.log("/ 　 へ＼ B");
-       　　 console.log("/　　/　＼＼ U");
-       　　console.log(" |　ノ　　 ヽ_つ L");
-       　　console.log("/　/ O");
-       　console.log(" /　/| U");
-       　console.log("(　(ヽ S");
-        　console.log("|　|、＼");
-        　console.log("| 丿 ＼ ⌒)");
-        　console.log("| |　　) /");
-    console.log("`ノ )　　Lﾉ");
-        gameLoop();
-
+            console.log('Guessed Guest List:', guessedGuestList);
+            gameLoop();
         });
-    }else{
+    }
+
+    // if(computerWord.display.length=computerWord.winning){
+    //     computerWord.WIN();
+    //     replay();
+    // }
+
+    if (computerWord.guesses==0){
         computerWord.showWordEndGame();
         computerWord.RIP();
+        replay();
     };
 };
+function replay() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            default: "False",
+            name: "playAgain",
+            message: "Want to try again?"
+
+        }
+    ]).then(function (resp) {
+        if (resp.playAgain) {
+            initialize();
+        } else {
+            goHomeToMommy();
+        }
+    })
+}
+function goHomeToMommy() {
+    console.log("-----------Harry Potter was born July 31, 1980------------");
+    console.log("-----------HARRY POTTER IS ABOUT TO ENTER HIS 40s------------");
+    console.log("-----DON'T LET HIM LIVE IN THE PAST. HE NEEDS TO MOVE ON...------")
+    console.log("------------------THANK YOU FOR HELPING HIM----------------------");
+
+}
 
 gameLoop();
+function initialize() {
+    //computerWord.guesses=3;
+    guessedGuestList = [];
+    computerWord.makeWord();
+    gameLoop();
+}
 
--- word.js
+++ word.js
@@ -6,37 +6,40 @@
 
 // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
 var letter = require("./letter");
-var win = 0;
+
 var Word = function (Rumpelstiltskin) {
     this.display = [];
+    this.winning=0;
     this.displayArray = [];
     this.letterObjects = [];
+    this.guesses = 5;
     this.autoAddSpace = function () {
         this.letterObjects.spaceCheck();
     }
-    this.addLife = function (guesses) {
-        guesses++;
-    }
+    // this.addLife = function (guesses) {
+    //     guesses++;
+    //     console.log("This is guesses in add life", guesses);}
+
     this.makeWord = function () {
         this.display = [];
+        this.guesses = 5;
+        //this.letterObjects =[];
+        //this.winning=0;
         for (i = 0; i < Rumpelstiltskin.length; i++) {
             var newLetter = new letter(Rumpelstiltskin.charAt(i));
             this.letterObjects.push(newLetter);
-            //console.log("This is console log in makeWord");
-            console.log(newLetter);
-            //console.log("Below is letterObjects.length");
-            //console.log(this.letterObjects.length);
-            //console.log("This.aletter Objects is below from Make word function");
-            //console.log(this.letterObjects);
+            //console.log(newLetter);
             this.display.push(this.letterObjects[i].char.toUpperCase());
         }
-        console.log("Below is just this.display from makeWord");
-        console.log(this.display);
-        console.log("WE are about to replace the above display with dashes");
+        // console.log("Below is just this.display from makeWord");
+        //console.log(this.display);
+        //console.log("We are about to replace the above display with dashes");
+        gameStartVisual();
+        console.log("|-------GUESS THE WORD!!--------|", "\n");
         this.replaceLettersWithUnderscores();
-    }
+        console.log("|-------GUESS THE WORD!!--------|", "\n");
 
-
+    };
     this.replaceLettersWithUnderscores = function () {
 
         for (i = 0; i < this.display.length; i++) {
@@ -44,47 +47,41 @@ var Word = function (Rumpelstiltskin) {
             var numOfElements = 1;
             if (this.letterObjects[i].char == " ") {
                 this.letterObjects[i].guessed = true;
-                win++;
-                console.log(this.letterObjects[i]);
-                console.log("this is win in replacing letters. Should be equal to how many spaces in word: ", win);
+                this.winning++;
+                console.log("This is this.winning in replace Letters", this.winning);
             }
             if (!this.letterObjects[i].guessed) {
                 this.display.splice(start_index, numOfElements, "_");
                 // this.display.splice(start_index,numOfElements,this.letterObjects[i].char);
             }
-
         }
-        console.log("Below is just this.display from replace Underscores");
+        //console.log("Below is just this.display from replace Underscores");
         console.log(this.display);
+        console.log( "\n");
     };
     this.replaceUnderscoresWithLetters = function () {
 
         for (i = 0; i < this.display.length; i++) {
-            console.log("The dislpay length is: ", this.display.length);
             var start_index = i;
             var numOfElements = 1;
 
             if (this.letterObjects[i].guessed) {
                 this.display.splice(start_index, numOfElements, this.letterObjects[i].char);
-                 //console.log("I tried to replace them with letterss!");
-                console.log("this is winin replace Underscores: ", win);
 
             }
-            if (win == this.display.length) {
-
-                console.log("this is win: ", win);
-
-                this.WIN();
-            }
+            
         }
-        console.log("The dislpay length is: ", this.display.length);
-        console.log("Win variable is at: ", win);
+        if (this.winning == this.display.length) {
+            this.WIN();
+        }
+        // console.log("The dislpay length is: ", this.display.length);
+        // console.log("Win variable is at: ", win);
         console.log(this.display);
     };
 
     this.showWordGuess = function (upper) {
         // console.log("Below is just this.display from showwordGuess");
-        console.log(this.display);
+        //console.log(this.display);
         //console.log("The above was the simple this.display in showWordGuess");
 
         for (i = 0; i < this.display.length; i++) {
@@ -92,43 +89,42 @@ var Word = function (Rumpelstiltskin) {
                 //console.log("This is upper in showWord Guess: ", upper);
                 //console.log("letter objects in showWordGuess", this.letterObjects[i].char)
                 this.letterObjects[i].guessed = true;
-                win++;
-                console.log("This is win in showWordGuess: ", win);
-                console.log("Below is the letterObjects with the guess changed");
-                console.log(this.letterObjects[i]);
+                this.winning++;
+                console.log("This is this.winning in show WOrd Guess", this.winning);
+                this.addGuesses();
+                // console.log("This is win in showWordGuess: ", win);
+                // console.log("Below is the letterObjects with the guess changed");
+                // console.log(this.letterObjects[i]);
             }
-
         }
         this.replaceUnderscoresWithLetters();
-
     };
-    // this.showDisplayArray = function () {
-    //     for (i = 0; i < this.letterObjects.length; i++) {
-    //         if (this.letterObjects[i].guess) {
-    //             this.displayArray.push(this.letterObjects[i].char);
-    //         }
-    //     }
-    //     var joined = this.displayArray.join("");
-    //     console.log("This is what you have so far" + joined);
-
-    // };
-
     this.showWordEndGame = function () {
         this.displayArray = [];
         for (i = 0; i < this.letterObjects.length; i++) {
-
             this.displayArray.push(this.letterObjects[i].char.toUpperCase());
         }
-        console.log("the following is ENDGAME displayarray");
+        // console.log("the following is ENDGAME displayarray");
         //console.log("The next was this.displayArray");
-        console.log(this.displayArray);
+        //console.log(this.displayArray);
         //console.log("The previous was this.displayArray");
         var joined = this.displayArray.join("");
-        console.log(" ~~~~~~~~~~~~");
-        console.log("This was the word you were looking for: " + joined);
-        console.log(" ~~~~~~~~~~~~");
+        console.log(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log("    This was the word you were looking for: " + joined);
+        console.log(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
     };
+    this.initialize=function(){
+
+        this.guesses=5;
+        this.display=[];
+    }
+    this.addGuesses = function () {
+        this.guesses=this.guesses+ 1;
+    }
+    this.subtractGuesses = function () {
+        this.guesses=this.guesses- 1;
+    }
     this.RIP = function () {
         console.log("\x1b[1m", "\x1b[31m", "You failed...", "\x1b[0m");
         console.log(" ___________");
@@ -141,8 +137,33 @@ var Word = function (Rumpelstiltskin) {
     };
     this.WIN = function () {
         console.log("You did it!! You have proved your nerdiness!");
-
-
+        console.log("＼＼ ＿");
+        console.log("＼(　•_•) F");
+        console.log(" <　⌒ヽ A");
+        console.log("/ 　 へ＼ B");
+        console.log("/　　/　＼＼ U");
+        console.log(" |　ノ　　 ヽ_つ L");
+        console.log("/　/ O");
+        console.log(" /　/| U");
+        console.log("(　(ヽ S");
+        console.log("|　|、＼");
+        console.log("| 丿 ＼ ⌒)");
+        console.log("| |　　) /");
+        console.log("`ノ )　　Lﾉ");
+    }
+    gameStartVisual = function () {
+        console.log("\n", "\n", "*~~~*~~~*~~~*~~~*~~~*~~~*~~~*")
+        console.log("      Hello there!");
+        console.log("          _██_");
+        console.log("        ‹(•¿•)›");
+        console.log("       .. (█)");
+        console.log("       .../ I");
+        console.log("    Care for a game?");
+        console.log("*~~~*~~~*~~~*~~~*~~~*~~~*~~~*", "\n");
+        console.log("\nEach blank represents an obscured letter you need to reveal.",
+            "\nYou only have a few guesses but if your guess reveals a letter",
+            "\nin the puzzle, you get another chance!");
+        console.log("Those are all the rules, now LET'S GO!!!!", "\n", "\n");
     }
 
 };
